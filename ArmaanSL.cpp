7505f604005eb66bd042924de8ab004640594e25
ArmaanHajar <armaanhajar@icloud.com>
2022-10-04 14:27:57 -0700

    test

--- ArmaanSL.cpp
+++ ArmaanSL.cpp
@@ -1,1 +1,19 @@
-// hello jeff
+#include <iostream>
+
+using namespace std;
+
+struct Student {
+  char firstName[80];
+  char lastName[80];
+  int studentID;
+  float gpa;
+}
+  
+int main() {
+  
+  return 0;
+}
+
+void newStudent() {
+  
+}
